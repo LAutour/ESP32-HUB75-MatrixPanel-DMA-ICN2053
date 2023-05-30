7ac609c75e7081df04e0978066cee94332754f3a
LAutour <LAutour@yandex.ru>
2023-05-30 16:16:18 +0300

    Update ESP32-HUB75-MatrixPanel-DMA-config.cpp
    add support 595 row decoder

--- ESP32-HUB75-MatrixPanel-DMA-config.cpp
+++ ESP32-HUB75-MatrixPanel-DMA-config.cpp
@@ -33,5 +33,6 @@ const hub75_cfg_t hub75_cfg_default= {
   .color_depth = PIXEL_COLOR_DEPTH, 
   .double_buff = DOUBLE_BUFF,
   .double_dma_buff = DOUBLE_DMA_BUFF,
+  .decoder_595 = DECODER_595,
   //  .latch_blanking = LAT_BLANKING,
 };
